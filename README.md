aec58b69b498ad9918d88b3fa2a49e6f8cdc18f7
Nivedha <86701661+nivedharadha@users.noreply.github.com>
2025-05-24 20:17:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# microservice-springboot-h2
+
---
+++ src/test/java/com/example/microservice_springboot_h2/microservice_springboot_h2/MicroserviceSpringbootH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice_springboot_h2.microservice_springboot_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringbootH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
